34923a65cd72c2756c8b6705e3855ee82c211b42
Sebastian Borsch <sebastian.borsch1996@gmail.com>
2023-07-01 17:04:03 +0200

    Fixed test for ARM and Linux, first Test green baby!

--- modules/jarep-ecs/tests/entitygeneratortests.cpp
+++ modules/jarep-ecs/tests/entitygeneratortests.cpp
@@ -1,12 +1,15 @@
 //
 // Created by Sebastian Borsch on 01.07.23.
 //
-
+#if __APPLE__
 #include <catch2/catch_test_macros.hpp>
+#else
+#include <catch2/catch.hpp>
 #include "../src/entitygenerator.hpp"
+#endif
 
 TEST_CASE("Add new entity"){
     auto *eg = new EntityGenerator();
     Entity newEntity = eg->createEntity();
-    REQUIRE(newEntity == 1);
+    REQUIRE(newEntity == 0);
 }
