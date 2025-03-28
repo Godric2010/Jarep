b9af7f71301a2d39d7a0970a11b2e308928cb8d1
Sebastian <sebastian.borsch1996@gmail.com>
2025-03-28 19:47:21 +0100

    Added basic VulkanRenderPass class to namespace

---
+++ src/Rendering/src/rendering/VulkanRenderPass.cpp
@@ -0,0 +1,81 @@
+//
+// Created by Godri on 3/28/2025.
+//
+
+#include "VulkanRenderPass.hpp"
+#include <stdexcept>
+
+using namespace JAREP::Rendering::Rendering;
+
+VulkanRenderPass::VulkanRenderPass(VkDevice device, const RenderPassConfig&config) {
+	m_device = device;
+	m_config = config;
+	m_renderPass = VK_NULL_HANDLE;
+
+	createRenderPass();
+}
+
+VulkanRenderPass::~VulkanRenderPass() {
+	if (m_renderPass != VK_NULL_HANDLE) {
+		vkDestroyRenderPass(m_device, m_renderPass, nullptr);
+	}
+}
+
+VkRenderPass VulkanRenderPass::get() const {
+	return m_renderPass;
+}
+
+void VulkanRenderPass::createRenderPass() {
+	VkAttachmentDescription colorAttachment = {};
+	colorAttachment.format = m_config.colorFormat;
+	colorAttachment.samples = m_config.samples;
+	colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+	colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+	colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+	colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+	colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+	colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+	VkAttachmentReference colorAttachmentRef = {};
+	colorAttachmentRef.attachment = 0;
+	colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+	std::vector<VkAttachmentDescription> attachments = {colorAttachment};
+	std::vector<VkAttachmentReference> colorAttachmentReferences = {colorAttachmentRef};
+
+	VkAttachmentReference depthStencilAttachmentRef = {};
+	if (m_config.depthFormat.has_value()) {
+		VkAttachmentDescription depthStencilAttachment = {};
+		depthStencilAttachment.format = m_config.depthFormat.value();
+		depthStencilAttachment.samples = m_config.samples;
+		depthStencilAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+		depthStencilAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+		depthStencilAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+		depthStencilAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+		depthStencilAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		depthStencilAttachment.finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+
+		depthStencilAttachmentRef.attachment = static_cast<uint32_t>(attachments.size());
+		depthStencilAttachmentRef.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+
+		attachments.push_back(depthStencilAttachment);
+	}
+
+	VkSubpassDescription subpass = {};
+	subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+	subpass.colorAttachmentCount = static_cast<uint32_t>(colorAttachmentReferences.size());
+	subpass.pColorAttachments = colorAttachmentReferences.data();
+	if (m_config.depthFormat.has_value()) {
+		subpass.pDepthStencilAttachment = &depthStencilAttachmentRef;
+	}
+	VkRenderPassCreateInfo renderPassInfo = {};
+	renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+	renderPassInfo.attachmentCount = static_cast<uint32_t>(attachments.size());
+	renderPassInfo.pAttachments = attachments.data();
+	renderPassInfo.subpassCount = 1;
+	renderPassInfo.pSubpasses = &subpass;
+
+	if (vkCreateRenderPass(m_device, &renderPassInfo, nullptr, &m_renderPass) != VK_SUCCESS) {
+		throw std::runtime_error("failed to create render pass!");
+	}
+}
---
+++ src/Rendering/src/rendering/VulkanRenderPass.hpp
@@ -0,0 +1,45 @@
+//
+// Created by Godri on 3/28/2025.
+//
+
+#pragma once
+#include <vulkan/vulkan.hpp>
+#include <optional>
+
+namespace JAREP::Rendering::Rendering {
+	struct RenderPassConfig {
+		VkFormat colorFormat;
+		std::optional<VkFormat> depthFormat = std::nullopt;
+		VkSampleCountFlagBits samples = VK_SAMPLE_COUNT_1_BIT;
+	};
+
+	/**
+	 * @class VulkanRenderPass
+	 * @brief Encapsulates the creation and configuration of a Vulkan render pass object.
+	 *
+	 * The VulkanRenderPass class defines how framebuffers are used during rendering,
+	 * including which attachments are involves, their formats, load/store operations,
+	 * and layout transitions. This class currently supports a basic single-subpass
+	 * configuration and is designed to be extendable for depth, multisampling, and
+	 * multiple attachments in the future.
+	 */
+	class VulkanRenderPass {
+		public:
+			explicit VulkanRenderPass(VkDevice device, const RenderPassConfig&config);
+
+			~VulkanRenderPass();
+
+			VulkanRenderPass(const VulkanRenderPass&) = delete;
+
+			VulkanRenderPass& operator=(const VulkanRenderPass&) = delete;
+
+			VkRenderPass get() const;
+
+		private:
+			void createRenderPass();
+
+			VkDevice m_device;
+			RenderPassConfig m_config;
+			VkRenderPass m_renderPass;
+	};
+}
